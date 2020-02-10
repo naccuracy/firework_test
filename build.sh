e57d3670fe8958d5647f5d68e60c511d02b57c74
Nick <klepikov.nick@yandex.ru>
2020-02-10 23:21:51 +0300

    (wip) build script implementation

--- build.sh
+++ build.sh
@@ -1,6 +1,43 @@
 #!/bin/bash
-LIBS="../libs"
+LIBS="./libs"
+DEST="./build"
+
+[ -d $LIBS ] || {
+    mkdir -pv $LIBS
+    mkdir -pv $LIBS/include
+    mkdir -pv $LIBS/libs
+}
+[ -d $DEST ] || {
+    mkdir -pv $DEST
+}
+[ -z $(which curl) ] && {
+    echo "curl not found, please install it to continue..."
+    exit 1
+}
+
+LIBPNG="libpng-1.6.37"
+LIBZLIB="zlib-1.2.11"
+TGZ=".tar.gz"
+pushd $LIBS
+[ -f $LIBPNG$TGZ ] || {
+    curl -O https://netcologne.dl.sourceforge.net/project/libpng/libpng16/1.6.37/$LIBPNG$TGZ
+    tar -xzf $LIBPNG$TGZ
+}
+[ -f $LIBZLIB$TGZ ] || {
+    curl -O http://www.zlib.net/$LIBZLIB$TGZ
+    tar -xzf $LIBZLIB$TGZ
+}
+
+[ -d $LIBPNG ] && {
+    echo "TODO: compile $LIBPNG"
+}
+[ -d $LIBZLIB ] && {
+    echo "TODO: compile $LIBZLIB"
+}
+
+popd
+
 SRC="mTexture.cpp main.cpp firework.cpp fpoint.cpp mWidget.cpp"
-g++ -std=c++0x -I $LIBS/libpng -I $LIBS/zlib -pthread -Wall -g $SRC -lpthread -lGL -lGLU -lX11 $LIBS/libpng/libpng16.a $LIBS/zlib/libz.a -o 1;
+#g++ -std=c++0x -I $LIBS/libpng -I $LIBS/zlib -pthread -Wall -g $SRC -lpthread -lGL -lGLU -lX11 $LIBS/libpng/libpng16.a $LIBS/zlib/libz.a -o 1;
 echo "Done!";
 exit 0;
