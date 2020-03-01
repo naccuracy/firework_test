ce4b3015cf49a035d0b31bb56a2d257f4efa66ee
Nick <klepikov.nick@yandex.ru>
2020-03-01 09:24:09 +0300

    do not rebuild libs always

--- build.sh
+++ build.sh
@@ -26,6 +26,7 @@ function get_cpu_cores_count(){
     echo ${cores}
 }
 ROOT_DIR=$(pwd)
+CMAKE_GENERATOR="Unix Makefiles"
 ############################
 LIBS="${ROOT_DIR}/libs"
 DEST="${ROOT_DIR}/build"
@@ -48,15 +49,23 @@ LIBZLIB="zlib-1.2.11"
 LIBGLFW="glfw-3.3.2"
 LIBSBUILD="$DEST/libs"
 
+PNG_LIB_FILE="${LIBS}/lib/libpng16"
+Z_LIB_FILE="${LIBS}/lib/libz"
+GLFW_LIB_FILE="${LIBS}/lib/libglfw3"
+
 SOSUFFIX="so"
 ASUFFIX="a"
 if [[ "$(on_windows)" == "yes" ]]; then
     SOSUFFIX="dll"
     ASUFFIX="lib"
+    Z_LIB_FILE="${LIBS}/lib/libzlibstatic"
 fi
 if [[ "$(on_macosx)" == "yes" ]]; then
     SOSUFFIX="dylib"
 fi
+PNG_LIB_FILE+=".${ASUFFIX}"
+Z_LIB_FILE+=".${ASUFFIX}"
+GLFW_LIB_FILE+=".${ASUFFIX}"
 
 [ -d $LIBSBUILD ] || { 
     mkdir -pv $LIBSBUILD
@@ -79,21 +88,23 @@ pushd $LIBSBUILD
 }
 
 [ -d $LIBZLIB ] && {
-    [ -f ${LIBS}/lib/libz.${ASUFFIX} ] || {
+    [ -f ${Z_LIB_FILE} ] || {
+        echo "build ${Z_LIB_FILE}"
         [ -d ${LIBZLIB}_build ] || { 
             mkdir -v ${LIBZLIB}_build
         }
-        cmake -G "Unix Makefiles" -S ${LIBZLIB} -B ${LIBZLIB}_build -DCMAKE_INSTALL_PREFIX="${LIBS}" -DSKIP_INSTALL_FILES=1
+        cmake -G "${CMAKE_GENERATOR}" -S ${LIBZLIB} -B ${LIBZLIB}_build -DCMAKE_INSTALL_PREFIX="${LIBS}" -DSKIP_INSTALL_FILES=1
         cmake --build ${LIBZLIB}_build --config Release --target install
         rm ${LIBS}/lib/*${SOSUFFIX}*
     }
 }
 [ -d $LIBPNG ] && {
-    [ -f ${LIBS}/lib/libpng16.${ASUFFIX} ] || {
+    [ -f ${PNG_LIB_FILE} ] || {
+        echo "build ${PNG_LIB_FILE}"
         [ -d ${LIBPNG}_build ] || { 
             mkdir -v ${LIBPNG}_build
         }
-        cmake -G "Unix Makefiles" -S ${LIBPNG} -B ${LIBPNG}_build -DCMAKE_INSTALL_PREFIX="${LIBS}" -DSKIP_INSTALL_FILES=1 -DSKIP_INSTALL_PROGRAMS=1 -DPNG_SHARED=0 -DPNG_TESTS=0 -DPNG_BUILD_ZLIB=1 -DZLIB_INCLUDE_DIR="${LIBS}/include" -DZLIB_LIBRARY="${LIBS}/lib/libz.${ASUFFIX}"
+        cmake -G "${CMAKE_GENERATOR}" -S ${LIBPNG} -B ${LIBPNG}_build -DCMAKE_INSTALL_PREFIX="${LIBS}" -DSKIP_INSTALL_FILES=1 -DSKIP_INSTALL_PROGRAMS=1 -DPNG_SHARED=0 -DPNG_TESTS=0 -DPNG_BUILD_ZLIB=1 -DZLIB_INCLUDE_DIR="${LIBS}/include" -DZLIB_LIBRARY="${LIBS}/lib/libz.${ASUFFIX}"
         cmake --build ${LIBPNG}_build --config Release --target install
         rm -rf ${LIBS}/lib/libpng
         rm -rf ${LIBS}/bin
@@ -102,11 +113,12 @@ pushd $LIBSBUILD
     }
 }
 [ -d $LIBGLFW ] && {
-    [ -f ${LIBS}/lib/libglfw3.${ASUFFIX} ] || {
+    [ -f ${GLFW_LIB_FILE} ] || {
+        echo "build ${GLFW_LIB_FILE}"
         [ -d ${LIBGLFW}_build ] || { 
             mkdir -v ${LIBGLFW}_build
         }
-        cmake -G "Unix Makefiles" -S ${LIBGLFW} -B ${LIBGLFW}_build -DCMAKE_INSTALL_PREFIX="${LIBS}" -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF
+        cmake -G "${CMAKE_GENERATOR}" -S ${LIBGLFW} -B ${LIBGLFW}_build -DCMAKE_INSTALL_PREFIX="${LIBS}" -DGLFW_BUILD_EXAMPLES=OFF -DGLFW_BUILD_TESTS=OFF -DGLFW_BUILD_DOCS=OFF
         cmake --build ${LIBGLFW}_build --config Release --target install 
     }
 }
@@ -118,13 +130,11 @@ PROJBUILD="$DEST/project"
 }
 
 [ -f "${PROJBUILD}/firework" ] || {
-    cmake -G "Unix Makefiles" -S ./ -B ${PROJBUILD}
+    cmake -G "${CMAKE_GENERATOR}" -S ./ -B ${PROJBUILD}
     cmake --build ${PROJBUILD} --config Release
 }
 
-[ -f "${PROJBUILD}/firework" ] && {
-    cp -v ${PROJBUILD}/firework ./firework
-}
 
-echo "Done!";
+echo "path to firework binary: ${PROJBUILD}"
+echo "done"
 exit 0;
